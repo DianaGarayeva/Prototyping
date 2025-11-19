ba46effc86037baa03e1d8e9cce34c412840e328
Diana <digar1305@gmail.com>
2025-11-19 12:07:25 +0500

    Added Laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _Speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _Speed * Time.deltaTime);
+
+        if(transform.position.y > 11f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 8.0f; 
+    private float _speed = 8.0f;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    [SerializeField]
+    private float _canFire = -1;
+
+    [SerializeField]
+    private GameObject _Laser_prefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +22,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            _canFire = Time.time + _fireRate;
+            Instantiate(_Laser_prefab, transform.position+new Vector3(0f, 0.8f, 0f), Quaternion.identity);
+        }
+        Motion();
+    }
+
+    void Motion()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
@@ -22,7 +39,7 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
         }
-        else if(transform.position.y < -3f)
+        else if (transform.position.y < -3f)
         {
             transform.position = new Vector3(transform.position.x, -3f, 0);
 
